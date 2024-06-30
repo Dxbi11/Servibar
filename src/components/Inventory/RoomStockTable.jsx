8d42aeb08c94f91c55e4c1ef8ce21e4e44ceed8c
David Herrera <jdha2015.dh@gmail.com>
2024-06-29 22:08:47 -0600

    Create RoomStockTable.jsx

---
+++ src/components/Inventory/RoomStockTable.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import {
+  Editable,
+  EditableInput,
+  EditableTextarea,
+  EditablePreview,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+
+const RoomStockTable = ({ hotelId }) => {
+  return (
+    <div>
+      <Editable defaultValue="Take some chakra">
+        <EditablePreview />
+        <EditableInput />
+      </Editable>
+
+      <TableContainer>
+        <Table variant="striped" colorScheme="teal">
+          <TableCaption>Imperial to metric conversion factors</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>To convert</Th>
+              <Th>into</Th>
+              <Th isNumeric>multiply by</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td>inches</Td>
+              <Td>millimetres (mm)</Td>
+              <Td isNumeric>25.4</Td>
+            </Tr>
+            <Tr>
+              <Td>feet</Td>
+              <Td>centimetres (cm)</Td>
+              <Td isNumeric>30.48</Td>
+            </Tr>
+            <Tr>
+              <Td>yards</Td>
+              <Td>metres (m)</Td>
+              <Td isNumeric>0.91444</Td>
+            </Tr>
+          </Tbody>
+          <Tfoot>
+            <Tr>
+              <Th>To convert</Th>
+              <Th>into</Th>
+              <Th isNumeric>multiply by</Th>
+            </Tr>
+          </Tfoot>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+};
+
+export default RoomStockTable;
