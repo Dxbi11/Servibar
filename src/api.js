8dc43f6dfbc1d4207089a28dd3a0bddf13f6e63b
David Herrera <jdha2015.dh@gmail.com>
2024-08-23 22:37:59 -0600

    Update api.js
    i am

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@
 
 import axios from 'axios';
 
-const API_URL = 'http://localhost:4000'; // Replace with your backend URL
+const API_URL = 'https://servibarex.onrender.com'; // Replace with your backend URL
 
 const api = axios.create({
   baseURL: API_URL,
